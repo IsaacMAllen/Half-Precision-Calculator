a634a5e2a84e1d1b230984af890eca826f161485
Isaac Allen <69442967+IsaacMAllen@users.noreply.github.com>
2020-12-16 14:15:29 -0500

    Added optional test method, split methods.

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+
 /**
  * Half-precision binary floating point to base-10 double converter. 
  * 
  * @author Isaac Allen 
- * @version 1.0.3
+ * @version 1.0.4
  */
 public class Calculator
 {
@@ -16,6 +17,10 @@ public class Calculator
     private int[] numArr = new int[16];
     private Scanner keyboard = new Scanner(System.in);
     private String stringNumber;
+    private int decimalPosition = 0;
+    private ArrayList<String> binValList = new ArrayList<>();
+    private double baseTenValue;
+    // private long testRun = 0;
    
     public Calculator()
     {
@@ -26,20 +31,24 @@ public class Calculator
     {
         do
         {
+            
             if (!exit)
             {
-                promptUser();
+                //String input = test();
+                promptUser(/*input*/);
                 filterInput();
             }    
-            if (parsed)
+            if (parsed && !exit)
             {
-                valueAssigner();
+                floatArrtoBaseTwoArrList();
+                baseTwoArrListToBaseTenDouble();
+                System.out.println(base10ValToString());
             }
         } while (!exit);
              
     }
 
-    public void promptUser()
+    public void promptUser(/*String input*/)
     {
         if (valid)
         {
@@ -55,6 +64,8 @@ public class Calculator
         {
             System.out.println("Invalid number, please re-enter or type exit.");
         }
+        //System.out.println(input);
+        //stringNumber = input;
         stringNumber = keyboard.nextLine();
         length = stringNumber.length();      
     }
@@ -97,174 +108,198 @@ public class Calculator
         }
     }
 
-    public void valueAssigner()
-    {
-        if (!exit)
+    // Initializes an arraylist (binValList) of the translated binary value from the 16 bit input.
+    public void floatArrtoBaseTwoArrList()
+    {              
+        double exponent = 0;  
+        int indexCoefDecrementer = 5;
+        int mantissaIndex = 2;
+        binValList.clear();
+        for (int i = 1; i < 6 ; i++ )
         {
-            ArrayList<String> binValList = new ArrayList<>();
-            double baseTenValue;
-            double denominator = 0;  
-            double exponent = 0;
-            double fraction = 0;
-            double nBeforeDecimal = 0;
-            double numerator = 0;            
-            double wholeValue = 0; 
-            int decimalPosition = 0;
-            int fractionalValueIndexDecrementer = 0;
-            int indexCoef = 0;
-            int indexCoefDecrementer = 5;
-            int indexDecrementer = 0;
-            int mantissaIndex = 2;
-            int sign = 1; 
-            int wholeValueIndexDecrementer = 0;
-            String binValString = "";
-            StringBuilder sb = new StringBuilder();
+            exponent += (numArr[i] * Math.pow(2, indexCoefDecrementer)) / 2;
+            indexCoefDecrementer--;
+        }
 
-            
-// Initializes an arraylist (binValList) of the translated binary value from the 16 bit input.
-            for (int i = 1; i < 6 ; i++ )
+        exponent -= 15;
+        binValList.add(0, "1");
+        binValList.add(1,".");
+        
+        for (int i = 6; i < 16; i++)
+        {
+            binValList.add(mantissaIndex, String.valueOf(numArr[i]));
+            mantissaIndex++;
+        }
+        if ((exponent + 1) < 0)
+        {
+            binValList.remove(1);
+            for(int i = 1; i <= ((exponent + 1) * -1); i++)
             {
-                exponent += (numArr[i] * Math.pow(2, indexCoefDecrementer)) / 2;
-                indexCoefDecrementer--;
+                binValList.add(0, "0");
             }
-
-            exponent -= 15;
-            binValList.add(0, "1");
-            binValList.add(1,".");
-            
-            for (int i = 6; i < 16; i++)
+            binValList.add(0, ".");
+        } else if (exponent > binValList.size())
             {
-                binValList.add(mantissaIndex, String.valueOf(numArr[i]));
-                mantissaIndex++;
-            }
-            if ((exponent + 1) < 0)
+                for (int i = 12; i < exponent + 1; i++)
+                {
+                    binValList.add(i, "0");
+                }        
+                binValList.remove(1);
+                binValList.add((int) exponent, ".");           
+            } else if (exponent < 0)
             {
                 binValList.remove(1);
-                for(int i = 1; i <= ((exponent + 1) * -1); i++)
+                for (int i = 0; i < -exponent - 1; i++)
                 {
                     binValList.add(0, "0");
                 }
                 binValList.add(0, ".");
-            } else if (exponent > binValList.size())
-                {
-                    for (int i = 12; i < exponent + 1; i++)
-                    {
-                        binValList.add(i, "0");
-                    }        
-                    binValList.remove(1);
-                    binValList.add((int) exponent, ".");           
-                } else if (exponent < 0)
-                {
-                    binValList.remove(1);
-                    for (int i = 0; i < -exponent - 1; i++)
-                    {
-                        binValList.add(0, "0");
-                    }
-                    binValList.add(0, ".");
-                }
+            }
 
-            decimalPosition = binValList.indexOf(".");
+        decimalPosition = binValList.indexOf(".");
 
-            if (exponent != decimalPosition && exponent > 0)
+        if (exponent != decimalPosition && exponent > 0)
+        { 
+            if ((int) exponent + 1 <= binValList.size())
             {
-                binValList.remove(decimalPosition);
                 binValList.add((int) exponent + 1, ".");
-            }
-
-            // Converts the binValList to a double.
-            decimalPosition = binValList.indexOf(".");
-            wholeValueIndexDecrementer = decimalPosition - 1;
-            fractionalValueIndexDecrementer = binValList.size() - 1;           
-            indexDecrementer = binValList.lastIndexOf("1"); 
-
-            for (int i = 0; i < binValList.size(); i++)
-            {               
-                if (i < decimalPosition)
-                {
-                    wholeValue += (Double.valueOf(binValList.get(wholeValueIndexDecrementer)) * Math.pow(2, i));
-                    nBeforeDecimal++;  
-                    wholeValueIndexDecrementer--;
-                }
-                if (indexDecrementer >= 0 && indexDecrementer > decimalPosition)
-                {
-                    if (fractionalValueIndexDecrementer > decimalPosition && !binValList.get(indexDecrementer).equals("."))
-                    {
-                        numerator += (Double.valueOf(binValList.get(indexDecrementer)) * Math.pow(2, indexCoef));
-                        indexCoef++;
-                        indexDecrementer--;
-                    } 
-                    fractionalValueIndexDecrementer--;
-                }
-            }
-            if (numArr[0] == 0)
-            {
-                sign = 1;
+                binValList.remove(decimalPosition);
             } else
             {
-                sign = -1;
+                binValList.add(".");
+                binValList.remove(decimalPosition);
             }
-
-            wholeValue *= sign;
-
-            if (decimalPosition != binValList.size() - 1 && numerator != 0)
-            {            
-                denominator = Math.pow(2, (binValList.lastIndexOf("1") - nBeforeDecimal));
-            }
-            if (denominator != 0)
-            {
-                fraction = numerator / denominator;
-                fraction *= sign;
-            }
-
-            baseTenValue = wholeValue + fraction;
-
-            // Initializes the sb StringBuilder object to the translated binary value.
-            if (numArr[0] == 1)
-            {
-                sb.append("-");
-            }
-            for (String s : binValList)
-            {
-                sb.append(s);
-            }
-            if (binValList.get(binValList.size() - 1).equals("."))
-            {
-                sb.append("0");
-            }
-
-            // Removes Trailing Zeroes from binary output string if any exist.
-            if (sb.lastIndexOf("1") > sb.indexOf(".") && sb.charAt(sb.length() - 1) == '0')
-            {
-                for (int trailingZeroIndex = sb.length() - 1; trailingZeroIndex > sb.lastIndexOf("1"); trailingZeroIndex--)
-                {
-                    sb.deleteCharAt(trailingZeroIndex);
-                }
-            }
-
-            // Inserts a zero in the first valued offset of sb if the base 10 value is missing an integer component.
-            if (sb.charAt(0) == '.' || (sb.charAt(0) == '-' && sb.charAt(1) == '.'))
-            {
-                if (sb.charAt(0) == '-' && sb.charAt(1) == '.')
-                {
-                    sb.insert(1, "0");
-                } else
-                {
-                    sb.insert(0, "0");
-                }
-            }
-
-            binValString = sb.toString();
-
-            System.out.println("Binary value = " + binValString);
-            System.out.println("Decimal value = " + baseTenValue);
-
-            valid = true;
-
+            
         }
     }
+
+    // Converts the binValList to a double.
+    public void baseTwoArrListToBaseTenDouble()
+    {           
+        double denominator = 0;
+        double fraction = 0;
+        double nBeforeDecimal = 0;
+        double numerator = 0;            
+        double wholeValue = 0;
+        int fractionalValueIndexDecrementer = 0;
+        int indexCoef = 0;
+        int indexDecrementer = 0;
+        int sign = 1; 
+        int wholeValueIndexDecrementer = 0;
+
+        decimalPosition = binValList.indexOf(".");
+        wholeValueIndexDecrementer = decimalPosition - 1;
+        fractionalValueIndexDecrementer = binValList.size() - 1;           
+        indexDecrementer = binValList.lastIndexOf("1"); 
+
+        for (int i = 0; i < binValList.size(); i++)
+        {               
+            if (i < decimalPosition)
+            {
+                wholeValue += (Double.valueOf(binValList.get(wholeValueIndexDecrementer)) * Math.pow(2, i));
+                nBeforeDecimal++;  
+                wholeValueIndexDecrementer--;
+            }
+            if (indexDecrementer >= 0 && indexDecrementer > decimalPosition)
+            {
+                if (fractionalValueIndexDecrementer > decimalPosition && !binValList.get(indexDecrementer).equals("."))
+                {
+                    numerator += (Double.valueOf(binValList.get(indexDecrementer)) * Math.pow(2, indexCoef));
+                    indexCoef++;
+                    indexDecrementer--;
+                } 
+                fractionalValueIndexDecrementer--;
+            }
+        }
+        if (numArr[0] == 0)
+        {
+            sign = 1;
+        } else
+        {
+            sign = -1;
+        }
+
+        wholeValue *= sign;
+
+        if (decimalPosition != binValList.size() - 1 && numerator != 0)
+        {            
+            denominator = Math.pow(2, (binValList.lastIndexOf("1") - nBeforeDecimal));
+        }
+        if (denominator != 0)
+        {
+            fraction = numerator / denominator;
+            fraction *= sign;
+        }
+
+        baseTenValue = wholeValue + fraction;
+    }
+
+    // Initializes the sb StringBuilder object to the translated binary value.
+    public String base10ValToString()
+    {
+
+        StringBuilder sb = new StringBuilder();
+        String binValString = "";
+        
+        if (numArr[0] == 1)
+        {
+            sb.append("-");
+        }
+        for (String s : binValList)
+        {
+            sb.append(s);
+        }
+        if (binValList.get(binValList.size() - 1).equals("."))
+        {
+            sb.append("0");
+        }
+
+        // Removes Trailing Zeroes from binary output string if any exist.
+        if (sb.lastIndexOf("1") > sb.indexOf(".") && sb.charAt(sb.length() - 1) == '0')
+        {
+            for (int trailingZeroIndex = sb.length() - 1; trailingZeroIndex > sb.lastIndexOf("1"); trailingZeroIndex--)
+            {
+                sb.deleteCharAt(trailingZeroIndex);
+            }
+        }
+
+        // Inserts a zero in the first valued offset of sb if the base 10 value is missing an integer component.
+        if (sb.charAt(0) == '.' || (sb.charAt(0) == '-' && sb.charAt(1) == '.'))
+        {
+            if (sb.charAt(0) == '-' && sb.charAt(1) == '.')
+            {
+                sb.insert(1, "0");
+            } else
+            {
+                sb.insert(0, "0");
+            }
+        }
+        
+        valid = true;
+        binValString = "Binary value = " + sb.toString();       
+        return binValString + "\nDecimal value = " + baseTenValue;    
+    }
+    
+    // optional test method.
+    // public String test()
+    // {
+    //     String out = "";
+        
+    //     if (testRun < 65536)
+    //     {
+    //         out = String.format("%016d", Long.parseLong(Long.toBinaryString(testRun)));
+    //         testRun++;
+    //         return out;
+    //     } else
+    //     {
+    //         return "exit";
+    //     } 
+    // }
+    
 
     public static void main(String[] args)
     {
         new Calculator();
     }
 }
+
